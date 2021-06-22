da9bb923d45053544ba04a4e85c557f7db612565
Mileto Di Marco <mileto.di.marco@howest.be>
2021-06-22 15:29:28 +0200

    Migrate to version 5.0

--- src/CoreCourse.ModelBindingSamples/Startup.cs
+++ src/CoreCourse.ModelBindingSamples/Startup.cs
@@ -1,50 +1,58 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace CoreCourse.ModelBindingSamples
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            app
-                .UseStaticFiles()
-                .UseMvc(routes =>
-                    {
-                        routes
-                        .MapRoute(
-                            name: "Home-actionWithRoute",
-                            template: "Home/ActionWithPrimitives/{firstName}/{surName}/{nickName}/{legs:int}/{isCaptain:bool}/{age:int?}",
-                            defaults: new { controller = "Home", action = "ActionWithPrimitives" }
-                        )
-                        .MapRoute(
-                            name: "default",
-                            template: "{controller}/{action}/{id?}",
-                            defaults: new { controller = "Home", action = "Index" }
-                        );
-                    })
-                ;
+            else
+            {
+                app.UseExceptionHandler("/Home/CustomError");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
