6e0cef9311109c9c7fbcca0819e8bbbe228d4355
Miledi75 <mileto.di.marco@howest.be>
2023-09-27 15:20:41 +0200

    Add wwwroot folder

---
+++ src/CoreCourse.ModelBindingSamples/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreCourse.ModelBindingSamples.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
